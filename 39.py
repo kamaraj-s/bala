009bfb6ac151926806115868ae315ad50e0b4631
kamaraj-s <52067980+kamaraj-s@users.noreply.github.com>
2019-06-28 14:42:53 +0530

    Update 39.py

--- 39.py
+++ 39.py
@@ -1,5 +1,7 @@
 FF=input()
 if FF in('a','A','e','E','i','I','o','O','u','U'):
   print('Vowel')
+elif FF in('b','B','c','C','d','D','f','F','g','G','h','H','j','J','k','K','l','L','m','M','n','N','p','P','q','Q','r','R','s','S','t','T','w','W','v','V','x','X','y','Y','z','Z'):
+	print('consonant')
 else:
-  print('consonant')
+  print('input error')
